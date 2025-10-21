191c2cb75ebbb5c135a9a8cb3778975998d2f32c
Miguel Marquina <mmarquina@ue003cofopri.gob.pe>
2025-10-21 11:46:04 -0500

    avance

---
+++ src/main/java/com/lp2/sisinventario/sistemainventario/SistemainventarioApplication.java
@@ -0,0 +1,13 @@
+package com.lp2.sisinventario.sistemainventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemainventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemainventarioApplication.class, args);
+	}
+
+}
