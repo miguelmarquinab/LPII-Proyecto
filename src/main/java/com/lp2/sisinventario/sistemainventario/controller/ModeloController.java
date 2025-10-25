bfc0e2a804d1f86128934760eecfd1cef6776764
Miguel Marquina <mmarquina@ue003cofopri.gob.pe>
2025-10-24 20:41:31 -0500

    Resolviendo errores

--- src/main/java/com/lp2/sisinventario/sistemainventario/controller/ModeloController.java
+++ src/main/java/com/lp2/sisinventario/sistemainventario/controller/ModeloController.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class ModeloController {
 
+
     private final ModeloService service;
 
     @GetMapping({"", "/"})
